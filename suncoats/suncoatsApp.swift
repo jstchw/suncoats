54a23a6b9f04ed57986215b6a99a9e4c4d62f115
jstchw <bobr703@gmail.com>
2022-11-14 21:57:12 +0000

    Initial Commit

---
+++ suncoats/suncoatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  suncoatsApp.swift
+//  suncoats
+//
+//  Created by Artem Bobrov on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct suncoatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
